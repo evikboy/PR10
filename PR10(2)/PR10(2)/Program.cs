306bdcae1db6edb49cd8281c90e9583b24801413
evikboy <Kislenko1950@gmail.com>
2021-05-07 21:02:15 +0300

    PR10

---
+++ PR10(2)/PR10(2)/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR10
+{
+    class Program
+    {
+        abstract class animal
+        {
+            protected string habitat;
+            protected string domain;
+            protected string kingdoms;
+
+            public abstract void input();
+            public abstract void output();
+        }
+
+        class Monkey : animal
+        {
+            private string typeMonkey;
+            private int amountBananaForDay;
+
+            public override void input()
+            {
+                Console.Write("Input habitat: ");
+                habitat = Console.ReadLine();
+                Console.Write("Input domain: ");
+                domain = Console.ReadLine();
+                Console.Write("Input kingdoms: ");
+                kingdoms = Console.ReadLine();
+                Console.Write("Input type of monkey: ");
+                typeMonkey = Console.ReadLine();
+                Console.Write("Input amount banana for monkey for 1 day: ");
+                amountBananaForDay = Convert.ToInt32(Console.ReadLine());
+            }
+
+            public override void output()
+            {
+                Console.WriteLine("Habitat: {0}\nDomain: {1}\nKingdoms: {2}\n" +
+                    "Type of monkey: {3}\nAmount banana for monkey for 1 day: {4}", habitat, domain, kingdoms, typeMonkey, amountBananaForDay);
+            }
+        }
+
+        class Lion : animal
+        {
+            private double kgMeatforweek;
+            private string partner;
+
+            public override void input()
+            {
+                Console.Write("Input habitat: ");
+                habitat = Console.ReadLine();
+                Console.Write("Input domain: ");
+                domain = Console.ReadLine();
+                Console.Write("Input kingdoms: ");
+                kingdoms = Console.ReadLine();
+                Console.Write("Input kg of meat for 1 week: ");
+                kgMeatforweek = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Input having a partner: ");
+                partner = Console.ReadLine();
+
+            }
+
+            public override void output()
+            {
+                Console.WriteLine("Habitat: {0}\nDomain: {1}\nKingdoms: {2}\n" +
+                    "Kg of meat for 1 week: {3}\nHaving a partner: {4}", habitat, domain, kingdoms, kgMeatforweek, partner);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Monkey monkey = new Monkey();
+            monkey.input();
+            Console.WriteLine("\n");
+            monkey.output();
+
+            Console.WriteLine("\n");
+
+            Lion lion = new Lion();
+            lion.input();
+            Console.WriteLine("\n");
+
+            lion.output();
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ PR10/PR10/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR10
+{
+    class Program
+    {
+        abstract class Plane
+        {
+            protected string name;
+            protected double engineCapacity;
+            protected double fullSpeed;
+
+            public abstract void input();
+            public abstract void output();
+        }
+
+        class Destroyer : Plane
+        {
+            private int amountRockets;
+            private string mission;
+
+            public override void input()
+            {
+                Console.Write("Input name: ");
+                name = Console.ReadLine();
+                Console.Write("Input engine capacity: ");
+                engineCapacity = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Input max speed: ");
+                fullSpeed = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Input amount of rockets: ");
+                amountRockets = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Input mission: ");
+                mission = Console.ReadLine();
+            }
+
+            public override void output()
+            {
+                Console.WriteLine("Name: {0}\nEngine capacity: {1}\nMax speed: {2}\n" +
+                    "Amount of rockets: {3}\nMission: {4}", name, engineCapacity,fullSpeed, amountRockets, mission);
+            }
+        }
+
+        class PassangerLiner : Plane
+        {
+            private int amountPeople;
+            private double priceTicket;
+
+            public override void input()
+            {
+                Console.Write("Input name: ");
+                name = Console.ReadLine();
+                Console.Write("Input engine capacity: ");
+                engineCapacity = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Input max speed: ");
+                fullSpeed = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Input amount of people: ");
+                amountPeople = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Input price ticket: ");
+                priceTicket = Convert.ToDouble(Console.ReadLine());
+            }
+
+            public override void output()
+            {
+                Console.WriteLine("Name: {0}\nEngine capacity: {1}\nMax speed: {2}\n" +
+                    "Amount of people: {3}\nPrice ticket: {4}", name, engineCapacity, fullSpeed, amountPeople, priceTicket);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Destroyer destroyer = new Destroyer();
+            destroyer.input();
+            Console.WriteLine("\n");
+            destroyer.output();
+
+            Console.WriteLine("\n");
+
+            PassangerLiner passangerLiner = new PassangerLiner();
+            passangerLiner.input();
+            Console.WriteLine("\n");
+
+            passangerLiner.output();
+
+            Console.ReadLine();
+        }
+
+    }
+}
